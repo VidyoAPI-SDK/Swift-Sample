7d10a05e035c7544474c4dd42f950671fdf8c6e9
Rahul Mangla <rahul.mangla@enghouse.com>
2025-10-02 21:10:29 +0530

    [x] Upgrade to work with 26.1.0 GA

--- VidyoConnector/SupportingFiles/VidyoConnectorVersion_.sh
+++ VidyoConnector/SupportingFiles/VidyoConnectorVersion_.sh
@@ -27,8 +27,8 @@
 COMMENTBLOCK
 
 # NOTE: Build tools/scripts automatically rewrite lines below to update version.
-VIDYO_XCODE_CFBundleShortVersionString="24.2.0"
-VIDYO_XCODE_CFBundleVersion="17"
+VIDYO_XCODE_CFBundleShortVersionString="26.1.0"
+VIDYO_XCODE_CFBundleVersion="3"
 
 # Modify relevant values in the build-generated Property List.
 # - Update what Apple calls the "version number" and the "build number".
